a2ea7e22a6eb36b7ce27acba41e7957b5330bcde
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-08 11:36:37 +0900

    Title: 숫자 사각형을 출력하는 함수 2, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240108/숫자 사각형을 출력하는 함수 2/function-outputs-numeric-square-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =1; i<n+1; i++){
+            for(int j=1; j<n*2+1; j*=2){
+                System.out.print(i*j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
