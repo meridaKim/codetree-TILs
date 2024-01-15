a68eb671382278b560c08e172fcb40dcaf06b800
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-15 19:45:54 +0900

    Title: 최소 와이파이 수, Time: 108ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240115/최소 와이파이 수/minimum-number-of-wifi.java
+++ 240115/최소 와이파이 수/minimum-number-of-wifi.java
@@ -9,10 +9,19 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int [] arr = new int [n];
         st = new StringTokenizer(br.readLine());
+        int zero =0;
         for(int i =0; i<n; i++){
             arr[i]=Integer.parseInt(st.nextToken());
+            if(arr[i]==0){
+                zero++;
+            }
         }
-        int wifi = n/(m*2+1)+n%(m*2+1);
+        int wifi =0;
+        if(m>0){
+            wifi = n/(m*2+1)+n%(m*2+1);
+        }
+        wifi = n-zero;
+        
         System.out.print(wifi);
 
     }
