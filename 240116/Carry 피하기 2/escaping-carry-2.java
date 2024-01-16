7450ae118ea5533eccc67f98fb078ff58f039674
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-16 15:13:46 +0900

    Title: Carry 피하기 2, Time: 110ms, Memory: 8MB, Status: Passed - CodeTree

--- 240116/Carry 피하기 2/escaping-carry-2.java
+++ 240116/Carry 피하기 2/escaping-carry-2.java
@@ -13,7 +13,7 @@ public class Main {
                 for(int k = j+1; k<arr.length; k++){
                     boolean carry = false;
                     if(arr[i]%10+arr[j]%10+arr[k]%10>=10) carry=true;
-                    if(arr[i]%100/10+arr[j]%101/10+arr[k]%100/10>=10) carry=true;
+                    if(arr[i]%100/10+arr[j]%100/10+arr[k]%100/10>=10) carry=true;
                     if(arr[i]%1000/100+arr[j]%1000/100+arr[k]%1000/100>=10) carry=true;
                     if(arr[i]%10000/1000+arr[j]%10000/1000+arr[k]%10000/1000>=10) carry=true;
                     if(carry==false){
