90d18da21ae2e60b99cab4ca2a9cae30bc6cd31b
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-18 15:01:55 +0900

    Title: G or H 2, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240118/G or H 2/G-or-H-2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        char [] arr = new char[101];
+        int end =0;
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            int location = Integer.parseInt(st.nextToken());
+            end = Math.max(location, end);
+            arr[location] = st.nextToken().charAt(0);
+        }
+        int max =0;
+        int count;
+        for(int i=0; i<end+1; i++){
+            count=0;
+            for(int j = i; j<end+1; j++){
+                if(arr[j]=='G'){
+                    count++;
+                }if(arr[j]=='H'){
+                    count--;
+                }if(count==0) {
+                    max = Math.max(j-i+1,max);
+                    break;}
+            }
+        }
+        System.out.print(max);
+
+    }
+}
