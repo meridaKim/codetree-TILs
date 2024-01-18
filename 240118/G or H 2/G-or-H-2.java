fbb6b3808d3b1bf0293d47c8013bf356b918de4f
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-18 15:44:12 +0900

    Title: G or H 2, Time: 103ms, Memory: 9MB, Status: Passed - CodeTree

--- 240118/G or H 2/G-or-H-2.java
+++ 240118/G or H 2/G-or-H-2.java
@@ -5,30 +5,36 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        char [] arr = new char[101];
+        int [] arr = new int[101];
         int end =0;
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             int location = Integer.parseInt(st.nextToken());
             end = Math.max(location, end);
-            arr[location] = st.nextToken().charAt(0);
+            char p = st.nextToken().charAt(0);
+            if(p=='G') arr[location] = 1;
+            if(p=='H') arr[location] = 2;
         }
         int max =0;
-        int count;
-        for(int i=0; i<end+1; i++){
-            count=0;
-            for(int j = i+1; j<end+1; j++){
-                if(arr[j]=='G'){
-                    count++;
-                }if(arr[j]=='H'){
-                    count--;
-                }if(count==0) {
+        for(int i=0; i<arr.length; i++){
+    
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[i]==0|| arr[j]==0) continue;
+                int G =0;
+                int H =0;
+                for(int k=i; k<=j; k++){
+                    if(arr[k]==1){
+                        G++;
+                    }if(arr[k]==2){
+                        H++;
+                    }
+                }
+                if(G==0||H==0||G==H) {
                     max = Math.max(j-i,max);
-                    break;}
+                    }
             }
-            if(count!=0) max = 0;
         }
-        System.out.print(max);
+        System.out.println(max);
 
     }
 }
