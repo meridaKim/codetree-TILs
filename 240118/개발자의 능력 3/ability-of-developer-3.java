93d8cb6fa6a1a358b4b7621300537e88981cd9e9
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-18 19:23:42 +0900

    Title: 개발자의 능력 3, Time: 284ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240118/개발자의 능력 3/ability-of-developer-3.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        while(st.hasMoreTokens()){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        int [] arr = list.stream().mapToInt(i->i).toArray();
+        int diff= Integer.MAX_VALUE;
+        for(int i=0; i<arr.length; i++){
+            for(int j =i; j<arr.length; j++){
+                diff = Math.min(diff, makeTeam(i,j,arr));
+            }
+        }
+        System.out.print(diff);
+        
+    }
+    public static int makeTeam(int i , int j, int [] arr){
+        int sum1 = arr[i]+arr[j];
+        int sum2 =0;
+        for(int k=0; k<arr.length; k++){
+            sum2+=arr[k];
+        }
+        sum2 = sum2-sum1;
+        return Math.abs(sum1-sum2);
+        
+    }
+}
