9959cea122e90cbb9623eb24524fdc53a5848c84
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-18 19:24:21 +0900

    Title: 개발자의 능력 3, Time: 249ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240118/개발자의 능력 3/ability-of-developer-3.java
+++ 240118/개발자의 능력 3/ability-of-developer-3.java
@@ -11,7 +11,7 @@ public class Main {
         int [] arr = list.stream().mapToInt(i->i).toArray();
         int diff= Integer.MAX_VALUE;
         for(int i=0; i<arr.length; i++){
-            for(int j =i; j<arr.length; j++){
+            for(int j =i+1; j<arr.length; j++){
                 diff = Math.min(diff, makeTeam(i,j,arr));
             }
         }
