4063a9fc038efcba3b131141d14d00fb3875f7bd
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-18 20:20:12 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 178ms, Memory: 10MB, Status: Passed - CodeTree

--- 240118/두 가지로 열리는 자물쇠/a-two-way-lock.java
+++ 240118/두 가지로 열리는 자물쇠/a-two-way-lock.java
@@ -1,30 +1,37 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st ;
-        int [][] arr = new int[2][3];
-        for(int i=0; i<2; i++){
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-            arr[i][2] = Integer.parseInt(st.nextToken());
-            
-        }
-        int count=0;
-        for(int i =0; i<arr.length; i++){
-            for(int j =1; j<=n; j++){
-                for(int k=1; k<=n; k++){
-                    for(int l =1; l <=n; l++){
-                        if(Math.min(Math.abs(arr[i][0]-j),Math.abs(n-(j-arr[i][0])))<=2&&Math.min(Math.abs(arr[i][1]-k),Math.abs(n-(k-arr[i][1])))<=2&&Math.min(Math.abs(arr[i][2]-l),Math.abs(n-(l-arr[i][2])))<=2){
-                            count++;
-                        }
-                    }
+    public static int n;
+    public static int a, b, c;
+    public static int a2, b2, c2;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        a2 = sc.nextInt();
+        b2 = sc.nextInt();
+        c2 = sc.nextInt();
+        
+        // 모든 조합을 다 만들어 봅니다.
+        int cnt = 0;
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                for(int k = 1; k <= n; k++) {
+                    // 모든 자리가 주어진 첫 조합과의 거리가 2 이내인지 확인합니다.
+                    if((Math.abs(a - i) <= 2 || Math.abs(a - i) >= n - 2) && (Math.abs(b - j) <= 2 || Math.abs(b - j) >= n - 2) && 
+                       (Math.abs(c - k) <= 2 || Math.abs(c - k) >= n - 2))
+                        cnt++;
+                    
+                    // 모든 자리가 주어진 두번째 조합과의 거리가 2 이내인지 확인합니다.
+                    else if((Math.abs(a2 - i) <= 2 || Math.abs(a2 - i) >= n - 2) && (Math.abs(b2 - j) <= 2 || Math.abs(b2 - j) >= n - 2) && 
+                       (Math.abs(c2 - k) <= 2 || Math.abs(c2 - k) >= n - 2))
+                        cnt++;
                 }
-            }
-        }
-        System.out.print(count-1);
+        
+        System.out.print(cnt);
     }
 }
