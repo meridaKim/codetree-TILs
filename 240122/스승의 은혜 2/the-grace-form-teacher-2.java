4756824c36da6fea658d00e6722f4498c2e5b58f
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-22 20:13:54 +0900

    Title: 스승의 은혜 2, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240122/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            list.add(sc.nextInt());
+        }
+        Collections.sort(list);
+        int sum =0;
+        int count=0;
+        int max = Integer.MIN_VALUE;
+        boolean coupon = false;
+        for(int i=0; i<n; i++){
+            sum=0;
+            sum+=list.get(i);
+            coupon=false;
+            for(int j =0; j<n; j++){
+                if(i==j) continue;
+                if(sum<m) {
+                    sum+=list.get(j);
+                    count++;
+                    }
+                if(sum>m&&coupon==false){
+                    sum-=list.get(j);
+                    count--;
+                    sum+=list.get(j)/2;
+                    coupon=true;
+                    if(sum>m) break;
+                    count++;
+                } 
+            }
+            max = Math.max(count, max);
+            count=0;
+        }
+        System.out.print(max);
+    }
+}
